1422e18f62b7e421409112edb73bfcdaccf97fbb
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2024-08-23 15:20:29 +0900

    update: Board.tsx 팔로우 목록 무한 스크롤 구현, api/posts 수정, delete: api/dbConnect 삭제

--- app/(route)/api/posts/route.ts
+++ app/(route)/api/posts/route.ts
@@ -1,38 +1,58 @@
 import prisma from "prisma/prisma";
 
 export async function GET(req: Request) {
-  const allPosts = await prisma.posts.findMany({
-    orderBy: {
-      date: "desc",
-    },
-    include: {
-      like_post: true,
-      author: {
-        //추가 이유: 팔로우 클릭하고 검색창 클릭 시 follow의 값이 store에 저장이 안되서 추가함
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          pro_img: true,
-          followers: true,
-          following: {
-            include: {
-              following: {
-                select: {
-                  pro_img: true,
-                  name: true,
-                  rep: true,
-                  rep_motion_url: true,
-                  credit: true,
-                  badge_list: true,
-                  followers: {
-                    include: {
-                      follower: {
-                        select: {
-                          id: true,
-                          name: true,
-                          email: true,
-                          pro_img: true,
+  const searchParams = new URL(req.url).searchParams;
+  const limit = searchParams.get("limit");
+  const page = searchParams.get("page");
+  const userId = searchParams.get("userId");
+
+  const limitNumber = limit ? Number(limit) : 10;
+  const pageNumber = page ? Number(page) : 1;
+  const userIdNumber = Number(userId);
+
+  const offset = (pageNumber - 1) * limitNumber;
+
+  let allPosts;
+  if (userId) {
+    allPosts = await prisma.posts.findMany({
+      where: {
+        author: {
+          followers: {
+            some: {
+              follower_id: userIdNumber, // 로그인한 사용자의 ID를 기준으로 팔로잉한 사람들 필터링
+            },
+          },
+        },
+      },
+      include: {
+        like_post: true,
+        author: {
+          //추가 이유: 팔로우 클릭하고 검색창 클릭 시 follow의 값이 store에 저장이 안되서 추가함
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            pro_img: true,
+            followers: true,
+            following: {
+              include: {
+                following: {
+                  select: {
+                    pro_img: true,
+                    name: true,
+                    rep: true,
+                    rep_motion_url: true,
+                    credit: true,
+                    badge_list: true,
+                    followers: {
+                      include: {
+                        follower: {
+                          select: {
+                            id: true,
+                            name: true,
+                            email: true,
+                            pro_img: true,
+                          },
                         },
                       },
                     },
@@ -43,8 +63,58 @@ export async function GET(req: Request) {
           },
         },
       },
-    },
-  });
+      orderBy: {
+        date: "desc",
+      },
+    });
+  } else {
+    allPosts = await prisma.posts.findMany({
+      skip: offset,
+      take: limitNumber,
+      orderBy: {
+        date: "desc",
+      },
+      include: {
+        like_post: true,
+        author: {
+          //추가 이유: 팔로우 클릭하고 검색창 클릭 시 follow의 값이 store에 저장이 안되서 추가함
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            pro_img: true,
+            followers: true,
+            following: {
+              include: {
+                following: {
+                  select: {
+                    pro_img: true,
+                    name: true,
+                    rep: true,
+                    rep_motion_url: true,
+                    credit: true,
+                    badge_list: true,
+                    followers: {
+                      include: {
+                        follower: {
+                          select: {
+                            id: true,
+                            name: true,
+                            email: true,
+                            pro_img: true,
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+  }
   return new Response(
     JSON.stringify({
       allPosts,
